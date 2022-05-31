dc270dd2cbd349ecefbc2d55b2a629b7b881e2bc
Maria Kazmina <maryamm2017amm@yandex.ru>
2022-05-31 22:11:28 +0300

    change mongoose settings

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 app.use(express.json());

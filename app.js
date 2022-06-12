dd38ed3c190607b140895e2332d9145eb94be475
Maria Kazmina <maryamm2017amm@yandex.ru>
2022-06-12 17:09:01 +0300

    fix

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
+const {errors} = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const login = require('./routes/users');
 const auth = require('./middlewares/auth');
 
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -19,11 +19,16 @@ app.use(express.json());
 app.post('/signin', login);
 app.post('/signup', usersRouter);
 
+app.use(auth);
+// запросы ниже требуют авторизации
+
+app.use('/', auth, usersRouter);
+app.use('/cards', auth, cardRouter);
 
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const {statusCode = 500, message} = err;
 
   res
     .status(statusCode)
@@ -34,18 +39,11 @@ app.use((err, req, res, next) => {
         : message,
     });
 
-  next();
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Маршрут не найден' });
+  res.status(404).send({message: 'Маршрут не найден'});
 });
-
-app.use(auth);
-// запросы ниже требуют авторизации
-
-app.use('/', usersRouter);
-app.use('/cards', cardRouter);
 
 
 app.listen(PORT, () => {

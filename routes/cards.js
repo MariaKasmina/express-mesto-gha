39a7d6b3e231120e4fcaa09dc4cd8b0576725399
Maria Kazmina <maryamm2017amm@yandex.ru>
2022-06-13 11:31:15 +0300

    add Joi into card middleware

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const cardRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getCards,
   deleteCard,
@@ -9,12 +11,29 @@ const {
 
 cardRouter.get('/', getCards); // получение карточек
 
-cardRouter.delete('/:cardId', deleteCard); // удаление карточки
+cardRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard); // удаление карточки
 
-cardRouter.post('/', addCard); // добавление карточки
+cardRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().pattern(/^http[s]*:\/\/[a-z0-9.\-_~:/?#[\]@!$&'()*+,;=]+|www\.[a-z0-9.-_~:?#[\]@!$&'()*+,;=]+/).required(),
+  }),
+}), addCard); // добавление карточки
 
-cardRouter.put('/:cardId/likes', addLike); // добавление лайка
+cardRouter.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}),addLike); // добавление лайка
 
-cardRouter.delete('/:cardId/likes', removeLike); // удаление лайка
+cardRouter.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}),removeLike); // удаление лайка
 
 module.exports = cardRouter;
